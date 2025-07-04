bcaf1ed02b616dc3ae897af22388af86b7ef62d1
AntGV <angarval@hotmail.es>
2025-07-04 14:20:18 +0200

    Update

--- README.md
+++ README.md
@@ -12,3 +12,35 @@ Autor: Antonio García Valverde
 
 ---
 
+### Objetivo del proyecto
+Una compañía de alojamiento turístico plantea la elaboración de un modelo de predicción que clasifique las futuras reservas que reciban en posibles confirmaciones o cancelaciones, con el objetivo de anticiparse a las cancelaciones y poder aplicar mejores propuestas de marketing y promociones a sus clientes a partir de la previsión.
+La compañía facilita los datos de las reservas recibidas entre los años 2015-2018 para su análisis.
+
+### Datasets empleados
+Los datos se han extraído de las siguientes fuentes:
+* **[Hotel_Reservations]("https://www.kaggle.com/datasets/ahsan81/hotel-reservations-classification-dataset")**
+* **[Hotel_Bookings]("https://www.kaggle.com/datasets/moro146/hotel-bookings")**
+A lo largo del proyecto se ha llevado a cabo un proceso de adecuación de las variables contenidas en ambos para poder ser empleados en conjunto.
+Posteriormente se han transformado sus valores para una mejor evaluación del modelo.
+
+### Solución aportada
+La propuesta de solución es el desarrollo de un modelo supervisado clasificador, para predecir si las futuras reservas serán confirmadas o canceladas.
+
+### Estructura de directorios
+Los directorios incluidos en este proyecto siguen la siguiente estructura:
+
+ML_CANCELACION_HOTELERA ----------- Directorio raiz del proyecto.
+    src/ -------------------------------- Subdirectorio de almacenamiento. 
+        data/ ------------------------------- Carpeta que contiene los dataset de trabajo una vez modificados los dataset fuente.
+        data_sample/ ------------------------ Carpeta que incluye una muestra del dataset de trabajo (dos archivos, con y sin target), para su empleo posterior en pruebas.
+        img/ -------------------------------- En esta carpeta se guardan las imágenes empleadas en el proyecto.
+        models/ ----------------------------- Carpeta donde se almacenará para producción el modelo desarrollado.
+        notebooks/ -------------------------- Contiene los diferentes notebooks de trabajo y evaluación empleados en el proyecto.
+        utils/ ------------------------------ En este directorio se almacenan losarchivos con funciones recurrentes empleadas en el análisis.
+    main.ipynb ------------------------------ Notebook donde se incluye el proceso completo, paso a paso, de creación del modelo.
+    presentacion.pdf ------------------------ Archivo pdf con la presentación del proyecto.
+
+---
+
+
+    
--- src/utils/toolbox.py
+++ src/utils/toolbox.py
@@ -4,128 +4,6 @@ import seaborn as sns
 import pandas as pd
 from sklearn.preprocessing import OrdinalEncoder
 
-def pinta_distribucion_categoricas(df, columnas_categoricas, relativa=False, mostrar_valores=False, giro = 45):
-    num_columnas = len(columnas_categoricas)
-    num_filas = (num_columnas // 2) + (num_columnas % 2)
-
-    fig, axes = plt.subplots(num_filas, 2, figsize=(15, 5 * num_filas))
-    axes = axes.flatten() 
-
-    for i, col in enumerate(columnas_categoricas):
-        ax = axes[i]
-        if relativa:
-            total = df[col].value_counts().sum()
-            serie = df[col].value_counts().apply(lambda x: x / total)
-            sns.barplot(x=serie.index, y=serie, ax=ax, palette='viridis', hue = serie.index, legend = False)
-            ax.set_ylabel('Frecuencia Relativa')
-        else:
-            serie = df[col].value_counts()
-            sns.barplot(x=serie.index, y=serie, ax=ax, palette='viridis', hue = serie.index, legend = False)
-            ax.set_ylabel('Frecuencia')
-
-        ax.set_title(f'Distribución de {col}')
-        ax.set_xlabel('')
-        ax.tick_params(axis='x', rotation=giro)
-
-        if mostrar_valores:
-            for p in ax.patches:
-                height = p.get_height()
-                ax.annotate(f'{height:.2f}', (p.get_x() + p.get_width() / 2., height), 
-                            ha='center', va='center', xytext=(0, 9), textcoords='offset points')
-
-    for j in range(i + 1, num_filas * 2):
-        axes[j].axis('off')
-
-    plt.tight_layout()
-    plt.show()
-
-
-def tipifica_variables(df: pd.DataFrame, umbral_categoria: int, umbral_continua: float):
-    """
-    Recibe un DataFrame y parámetros para tipificar las columnas
-
-    Argumentos:
-    df: cualquier dataframe.
-    umbral_categoria: marca el límite a partir el cual se va a considerar una variable categorica.
-    umbral_continua: marca el límite a partir el cual se va a diferenciar entre numérica continua o discreta,
-
-    Retorna:
-    DataFrame: un dataframe nuevo, donde las columnas del de entrada se convierten en filas y cada columna refleja el tipo de la variable.
-    """
-    
-    filas = []
-    tipos = []
-
-    for col in df:
-        n_unique = df[col].nunique()
-        cardinalidad_porcentaje = (n_unique / len(df)) * 100
-
-        if n_unique == 2:
-            tipo = 'Binaria'
-
-        elif n_unique < umbral_categoria:
-            tipo = 'Categorica'
-
-        else:
-
-            if cardinalidad_porcentaje >= umbral_continua:
-                tipo = 'Numérica Continua'
-            else:
-                tipo = 'Numérica Discreta'
-
-        filas.append(col)
-        tipos.append(tipo)
-
-    return_df = pd.DataFrame({
-        'nombre_variable': filas,
-        'tipo_sugerido': tipos
-    })
-
-    return return_df
-
-
-def nombres_columnas(df_names):
-    df_names.rename(columns = {"is_canceled":"booking_status",
-                      "arrival_date_year":"arrival_year",
-                      "arrival_date_month":"arrival_month",
-                      "arrival_date_day_of_month":"arrival_date",
-                      "stays_in_weekend_nights":"no_of_weekend_nights",
-                      "stays_in_week_nights":"no_of_week_nights",
-                      "adults":"no_of_adults",
-                      "children":"no_of_children",
-                      "meal":"type_of_meal_plan",
-                      "market_segment":"market_segment_type",
-                      "is_repeated_guest":"repeated_guest",
-                      "previous_cancellations":"no_of_previous_cancellations",
-                      "previous_bookings_not_canceled":"no_of_previous_bookings_not_canceled",
-                      "reserved_room_type":"room_type_reserved",
-                      "required_car_parking_spaces":"required_car_parking_space",
-                      "total_of_special_requests":"no_of_special_requests",
-                      "adr":"avg_price_per_room",
-                      }, inplace = True)
-
-    df_names.rename(columns = {"no_of_adults":"adults",
-                        "no_of_children":"children",
-                        "no_of_weekend_nights":"weekend_nights",
-                        "no_of_week_nights":"week_nights",
-                        "type_of_meal_plan":"meal_plan",
-                        "required_car_parking_space":"parking",
-                        "room_type_reserved":"room_type",
-                        "lead_time":"lead_time",
-                        "arrival_year":"arrival_year",
-                        "arrival_month":"arrival_month",
-                        "arrival_date":"arrival_day",
-                        "market_segment_type":"ms_type",
-                        "repeated_guest":"repeated_guest",
-                        "no_of_previous_cancellations":"pre_cancel",
-                        "no_of_previous_bookings_not_canceled":"pre_not_cancel",
-                        "avg_price_per_room":"daily_price",
-                        "no_of_special_requests":"special_req",
-                        "booking_status":"canceled",
-                        }, inplace = True)
-
-    return df_names
-
 
 def total_transform(df_main):
     
@@ -198,3 +76,91 @@ def total_transform(df_main):
     df.drop(columns = ["arr_date"], inplace = True)
 
     return df
+
+
+def nombres_columnas(df_names):
+    df_names.rename(columns = {"is_canceled":"booking_status",
+                      "arrival_date_year":"arrival_year",
+                      "arrival_date_month":"arrival_month",
+                      "arrival_date_day_of_month":"arrival_date",
+                      "stays_in_weekend_nights":"no_of_weekend_nights",
+                      "stays_in_week_nights":"no_of_week_nights",
+                      "adults":"no_of_adults",
+                      "children":"no_of_children",
+                      "meal":"type_of_meal_plan",
+                      "market_segment":"market_segment_type",
+                      "is_repeated_guest":"repeated_guest",
+                      "previous_cancellations":"no_of_previous_cancellations",
+                      "previous_bookings_not_canceled":"no_of_previous_bookings_not_canceled",
+                      "reserved_room_type":"room_type_reserved",
+                      "required_car_parking_spaces":"required_car_parking_space",
+                      "total_of_special_requests":"no_of_special_requests",
+                      "adr":"avg_price_per_room",
+                      }, inplace = True)
+
+    df_names.rename(columns = {"no_of_adults":"adults",
+                        "no_of_children":"children",
+                        "no_of_weekend_nights":"weekend_nights",
+                        "no_of_week_nights":"week_nights",
+                        "type_of_meal_plan":"meal_plan",
+                        "required_car_parking_space":"parking",
+                        "room_type_reserved":"room_type",
+                        "lead_time":"lead_time",
+                        "arrival_year":"arrival_year",
+                        "arrival_month":"arrival_month",
+                        "arrival_date":"arrival_day",
+                        "market_segment_type":"ms_type",
+                        "repeated_guest":"repeated_guest",
+                        "no_of_previous_cancellations":"pre_cancel",
+                        "no_of_previous_bookings_not_canceled":"pre_not_cancel",
+                        "avg_price_per_room":"daily_price",
+                        "no_of_special_requests":"special_req",
+                        "booking_status":"canceled",
+                        }, inplace = True)
+
+    return df_names
+
+
+def tipifica_variables(df: pd.DataFrame, umbral_categoria: int, umbral_continua: float):
+    """
+    Recibe un DataFrame y parámetros para tipificar las columnas
+
+    Argumentos:
+    df: cualquier dataframe.
+    umbral_categoria: marca el límite a partir el cual se va a considerar una variable categorica.
+    umbral_continua: marca el límite a partir el cual se va a diferenciar entre numérica continua o discreta,
+
+    Retorna:
+    DataFrame: un dataframe nuevo, donde las columnas del de entrada se convierten en filas y cada columna refleja el tipo de la variable.
+    """
+    
+    filas = []
+    tipos = []
+
+    for col in df:
+        n_unique = df[col].nunique()
+        cardinalidad_porcentaje = (n_unique / len(df)) * 100
+
+        if n_unique == 2:
+            tipo = 'Binaria'
+
+        elif n_unique < umbral_categoria:
+            tipo = 'Categorica'
+
+        else:
+
+            if cardinalidad_porcentaje >= umbral_continua:
+                tipo = 'Numérica Continua'
+            else:
+                tipo = 'Numérica Discreta'
+
+        filas.append(col)
+        tipos.append(tipo)
+
+    return_df = pd.DataFrame({
+        'nombre_variable': filas,
+        'tipo_sugerido': tipos
+    })
+
+    return return_df
+
